1aa8ce79ed0038ce6a22b3450c6f56aa06a71b8d
johnsonlee <g.johnsonlee@gmail.com>
2015-09-08 17:08:49 +0800

    Improve javadoc for logging module

---
+++ logging/spi/src/main/java/com/beastbikes/logging/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides logger factory helpers.
+ *
+ * @since 1.0.0
+ */
+package com.beastbikes.logging;
---
+++ logging/spi/src/main/java/com/beastbikes/logging/spi/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Defines interfaces for logging
+ *
+ * @since v1.0.0
+ */
+package com.beastbikes.logging.spi;
